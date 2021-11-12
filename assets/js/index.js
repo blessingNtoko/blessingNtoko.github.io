2acb6fec0cacb0a879988268c2342c4a344867b4
Blessing <bntoko@gmail.com>
2021-11-12 18:28:27 +0200

    removed some code

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const nav = document.getElementById('navigate');
 const letterN = document.getElementById('changeColour');
-let submitted = false;
 
 document.addEventListener('scroll', () => {
     if (window.scrollY) {
