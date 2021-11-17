a9d8e05337961a13ab338a9ee051b25893acf3ec
Blessing <bntoko@gmail.com>
2021-11-17 18:46:38 +0200

    dealing with css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM Content Loaded');
-});
-
+let screenWidth = 0;
 const nav = document.getElementById('navigate');
 const letterN = document.getElementById('changeColour');
+const menu = document.getElementById('menu');
+const closeMenu = document.getElementById('closeMenu');
+const openMenu = document.getElementById('openMenu');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Content Loaded');
+
+});
 
 document.addEventListener('scroll', () => {
     if (window.scrollY) {
@@ -14,3 +19,15 @@ document.addEventListener('scroll', () => {
         letterN.classList.remove('white');
     }
 });
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.add('hiddenNav');
+    menu.classList.remove('divMenu');
+    openMenu.classList.remove('hiddenNav');
+});
+
+openMenu.addEventListener('click', () => {
+    menu.classList.add('divMenu');
+    menu.classList.remove('hiddenNav');
+    openMenu.classList.add('hiddenNav');
+})
